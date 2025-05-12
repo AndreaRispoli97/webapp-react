82a078d63153ed593853ad89371ed52fd90e9f56
AndreaRispoli97 <Andrea9715@outlook.it>
2025-05-12 15:54:58 +0200

    feat: add routing and pages for Home and MovieDetail and complete Milestone 2

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+
+
+
+function Home() {
+    return (
+        <>
+            <div>
+                <h1>Lista Film</h1>
+                <ul>
+                    <li>bbb</li>
+                    <li>aaaa</li>
+                    <li>dfff</li>
+                    <li>ggg</li>
+                    <li>hhrf</li>
+                </ul>
+            </div>
+        </>
+    )
+}
+
+export default Home
---
+++ src/pages/MovieDetail.jsx
@@ -0,0 +1,17 @@
+
+
+
+
+
+function MovieDetail() {
+    return (
+        <>
+            <div>
+                <h2>Dettagli film</h2>
+                <p>Descrizione</p>
+            </div>
+        </>
+    )
+}
+
+export default MovieDetail
